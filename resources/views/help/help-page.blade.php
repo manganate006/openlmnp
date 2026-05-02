274a09bd656f887454731de34e1b2e8698f1cd31
Jeremie <jeremie@mangi.fr>
2026-05-02 19:57:17 +0200

    feat: aide contextuelle adaptée par onglet sur la page bien + aide page aide
    - Le partial properties.blade.php affiche une aide différente selon l'onglet
      actif (Général, Surfaces, Location, Travaux, Mobilier, Composants)
    - Détection de l'onglet via Alpine.js (écoute des clics sur fi-tabs-item)
    - Ajout d'une aide humoristique pour la page d'aide elle-même

---
+++ resources/views/help/help-page.blade.php
@@ -0,0 +1,10 @@
+<div class="ctx-help">
+    <h3>Vous y êtes déjà !</h3>
+    <p>Vous avez cliqué sur le bouton d'aide... depuis la page d'aide. C'est un peu comme chercher ses lunettes alors qu'elles sont sur votre nez.</p>
+
+    <p>Tout ce dont vous avez besoin se trouve juste là, à gauche. Parcourez les sections du guide pour trouver votre réponse.</p>
+
+    <div class="ctx-tip">
+        <strong>Astuce :</strong> Sur les autres pages, ce panneau affiche une aide contextuelle adaptée à ce que vous êtes en train de faire. Essayez !
+    </div>
+</div>
